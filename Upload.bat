596d36a45c4a4318f05469c7330d76a933099e6a
caxmirror <627085892@qq.com>
2023-02-17 01:11:35 +0800

    ChineseOCR 1.0

---
+++ Upload.bat
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/caxmirror/ChineseOCR.git
+git config user.name "caxmirror"
+git config user.email "627085892@qq.com"
+git add .
+git commit -m "ChineseOCR 1.0"
+git push -u origin master
+pause
